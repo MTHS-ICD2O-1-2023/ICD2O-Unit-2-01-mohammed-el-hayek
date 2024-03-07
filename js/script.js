e658334a3909782db2026d8e638b569c196badf7
mohammed-el-hayek <159185058+mohammed-el-hayek@users.noreply.github.com>
2024-03-07 02:30:52 +0000

    update for 2-01

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+/* Created by: Mohammed El-Hayek
+* Created on: Mar 2024
+* This file contains the CSS for index.html
+*/
+
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML="Hello, World!"
+}
